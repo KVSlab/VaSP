1ee9fa393bb0bccb314152f9ed9f98a4867e10be
DB_Ubuntu <dbruneau280@gmail.com>
2022-11-30 13:25:58 -0500

    Add predeformation script

---
+++ preprocessing_mesh/PredeformFSIMesh.py
@@ -0,0 +1,47 @@
+import common_meshing
+import h5py
+from shutil import copyfile
+from numpy import genfromtxt
+import numpy as np
+import common_meshing
+
+# User inputs ----------------------------
+scaleFactor=-1.0  # This is the factor used to scale the deformation
+folder, mesh_name = common_meshing.read_command_line()
+
+# -----------------------------------
+disp_filepath = folder +"/"+ mesh_name +'/1/Visualization/displacement.h5'
+mesh_path = folder + '/mesh/' + mesh_name +".h5"
+# -----------------------------------------
+
+dt = 0.000679286
+t = 0.59
+
+index_t = int(np.round(t/dt))
+print(disp_filepath)
+
+vectorData = h5py.File(disp_filepath, "r") 
+ArrayName = 'VisualisationVector/' + str(index_t)	
+deformation = vectorData[ArrayName][:,:] # Important not to take slices of this array, slows code considerably... 
+
+predeformed_mesh_path=mesh_path.replace(".h5", "_"+str(scaleFactor)+'.h5')
+copyfile(mesh_path, predeformed_mesh_path)
+
+#f = HDF5File(mpi_comm_world(),'meshes/'+mesh_name, 'r')
+#mesh_file = Mesh()
+#f.read(mesh, 'mesh')
+
+vectorData = h5py.File(predeformed_mesh_path,'a')
+
+ArrayNames = ['boundaries/coordinates','mesh/coordinates','domains/coordinates']
+
+
+#hdf5_store = h5py.File("meshes/deformed_"+meshname+'.h5', "w")
+
+for ArrayName in ArrayNames:
+
+	vectorArray = vectorData[ArrayName]
+	modified = vectorData[ArrayName][:,:] + deformation*scaleFactor
+	vectorArray[...] = modified
+
+vectorData.close() 
