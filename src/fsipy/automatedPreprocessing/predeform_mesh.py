b893dd10ef115453b7781a3f6a60ff72167734f6
Johannes Ring <johannes.ring@gmail.com>
2023-08-17 11:49:00 +0200

    Switch to pathlib for paths, remove shutil.copyfile

--- src/fsipy/automatedPreprocessing/predeform_mesh.py
+++ src/fsipy/automatedPreprocessing/predeform_mesh.py
@@ -12,51 +12,40 @@ subsequent simulation steps.
 """
 
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
-from os import path
 import h5py
-from shutil import copyfile
-
+from pathlib import Path
 
 def parse_arguments():
     parser = ArgumentParser(description=__doc__, formatter_class=RawDescriptionHelpFormatter)
     parser.add_argument('--folder', type=str, help="Path to simulation results")
     return parser.parse_args()
 
-
 def predeform_mesh(folder_path):
     # Path to the displacement file
-    disp_path = path.join(folder_path, "Visualization", "displacement.h5")
-    mesh_path = path.join(folder_path, "Checkpoint", "mesh.h5")
+    disp_path = Path(folder_path) / "Visualization" / "displacement.h5"
+    mesh_path = Path(folder_path) / "Checkpoint" / "mesh.h5"
+    predeformed_mesh_path = mesh_path.with_name(mesh_path.stem + "_predeformed.h5")
 
     # Read the displacement file and get the displacement from the last time step
-    vectorData = h5py.File(disp_path, "r")
-    number_of_datasets = len(vectorData["VisualisationVector"].keys())
-    disp_array = vectorData[f"VisualisationVector/{number_of_datasets - 1}"][:, :]
+    with h5py.File(disp_path, "r") as vectorData:
+        number_of_datasets = len(vectorData["VisualisationVector"].keys())
+        disp_array = vectorData[f"VisualisationVector/{number_of_datasets - 1}"][:, :]
 
-    # Create a copy of the mesh file with a new name
-    predeformed_mesh_path = mesh_path.replace(".h5", "_predeformed.h5")
-    copyfile(mesh_path, predeformed_mesh_path)
+    # Open the new mesh file in read-write mode
+    with h5py.File(predeformed_mesh_path, 'r+') as vectorData:
+        # We modify the original geometry by adding the reverse of the displacement
+        # Hence, scaleFactor = -1.0
+        scaleFactor = -1.0
 
-    # Open the new mesh file in append mode
-    vectorData = h5py.File(predeformed_mesh_path, 'a')
-
-    # We modify the original geometry by adding the reverse of the displacement
-    # Hence, scaleFactor = -1.0
-    scaleFactor = -1.0
-
-    ArrayNames = ['mesh/coordinates', 'domains/coordinates', 'boundaries/coordinates']
-    for ArrayName in ArrayNames:
-        vectorArray = vectorData[ArrayName]
-        modified = vectorData[ArrayName][:, :] + disp_array * scaleFactor
-        vectorArray[...] = modified
-
-    vectorData.close()
-
+        ArrayNames = ['mesh/coordinates', 'domains/coordinates', 'boundaries/coordinates']
+        for ArrayName in ArrayNames:
+            vectorArray = vectorData[ArrayName]
+            modified = vectorData[ArrayName][:, :] + disp_array * scaleFactor
+            vectorArray[...] = modified
 
 def main():
     args = parse_arguments()
     predeform_mesh(args.folder)
-
 
 if __name__ == '__main__':
     main()
