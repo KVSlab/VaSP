a5408ac75b11f65ca8367db8334243af412a8d3d
johannesring <johannes.ring@gmail.com>
2023-10-03 12:54:21 +0200

    Restructure package imports to avoid import of unnecessary submodules

--- src/fsipy/automatedPostprocessing/__init__.py
+++ src/fsipy/automatedPostprocessing/__init__.py
@@ -1,2 +1,9 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
+
 """Submodule for automated post-processing"""
+
+from . import log_plotter
+
+__all__ = [
+    "log_plotter",
+]
--- src/fsipy/simulations/__init__.py
+++ src/fsipy/simulations/__init__.py
@@ -1,1 +1,5 @@
 from . import simulation_common
+
+__all__ = [
+    "simulation_common",
+]
