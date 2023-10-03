cfd2290b09146d252d031845573a60db9040cf53
johannesring <johannes.ring@gmail.com>
2023-10-03 22:55:25 +0200

    Move predeform mesh script to post-processing folder

--- src/fsipy/automatedPreprocessing/__init__.py
+++ src/fsipy/automatedPreprocessing/__init__.py
@@ -1,9 +1,7 @@
 from . import automated_preprocessing
 from . import preprocessing_common
-from . import predeform_mesh
 
 __all__ = [
     "automatedPreprocessing",
     "preprocessing_common",
-    "predeform_mesh",
 ]
