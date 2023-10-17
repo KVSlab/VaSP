d11bffd516b1b271e5a1c3ea8389b373519bc0ff
johannesring <johannes.ring@gmail.com>
2023-10-17 08:08:17 +0200

    Refactor pre-processing tests

--- tests/test_pre_processing.py
+++ tests/test_pre_processing.py
@@ -1,9 +1,54 @@
-import vtk
 from pathlib import Path
+
+import vtk
 from dolfin import Mesh, HDF5File, XDMFFile, FunctionSpace, Function
 from vampy.automatedPreprocessing.preprocessing_common import read_polydata
+
 from fsipy.automatedPreprocessing.automated_preprocessing import read_command_line, \
     run_pre_processing
+
+
+def copy_original_model_to_tmpdir(original_model_path, tmpdir):
+    """
+    Copy the original model to the tmpdir.
+    """
+    model_path = Path(tmpdir) / original_model_path.name
+    model_path.write_text(original_model_path.read_text())
+    return model_path
+
+
+def run_pre_processing_with_common_input(model_path, common_input):
+    """
+    Run pre-processing with the given common input.
+    """
+    run_pre_processing(**common_input)
+
+    # Check that mesh files are created
+    mesh_path_vtu = model_path.with_suffix(".vtu")
+    mesh_path_hdf5 = model_path.with_suffix(".h5")
+    assert mesh_path_vtu.is_file(), f"VTU mesh file not found at {mesh_path_vtu}"
+    assert mesh_path_hdf5.is_file(), f"HDF5 mesh file not found at {mesh_path_hdf5}"
+
+    # Check that mesh files are not empty
+    mesh_vtu = read_polydata(str(mesh_path_vtu))
+    mesh_hdf5 = Mesh()
+    try:
+        hdf5_file = HDF5File(mesh_hdf5.mpi_comm(), str(mesh_path_hdf5), "r")
+        hdf5_file.read(mesh_hdf5, "/mesh", False)
+    except Exception as e:
+        print(f"Error reading HDF5 mesh: {e}")
+
+    return model_path, mesh_vtu, mesh_hdf5
+
+
+def assert_mesh_sizes(mesh_vtu, mesh_hdf5, expected_num_points, expected_num_cells):
+    """
+    Assert that mesh sizes match the expected values.
+    """
+    assert mesh_vtu.GetNumberOfPoints() == expected_num_points, \
+        f"VTU mesh has {mesh_vtu.GetNumberOfPoints()} points, expected {expected_num_points}"
+    assert mesh_hdf5.num_cells() == expected_num_cells, \
+        f"HDF5 mesh has {mesh_hdf5.num_cells()} cells, expected {expected_num_cells}"
 
 
 def test_mesh_model_with_one_inlet(tmpdir):
@@ -12,13 +57,9 @@ def test_mesh_model_with_one_inlet(tmpdir):
     """
     # Define test data paths
     original_model_path = Path("tests/test_data/tube/tube.stl")
-    model_path = Path(tmpdir) / original_model_path.name
-    mesh_path_vtu = model_path.with_suffix(".vtu")
-    mesh_path_hdf5 = model_path.with_suffix(".h5")
-    edge_length_mesh_path = model_path.with_name(model_path.stem + "_edge_length.xdmf")
 
     # Copy the original model to tmpdir
-    model_path.write_text(original_model_path.read_text())
+    model_path = copy_original_model_to_tmpdir(original_model_path, tmpdir)
 
     # Define expected values
     expected_num_points = 3626
@@ -39,22 +80,13 @@ def test_mesh_model_with_one_inlet(tmpdir):
         )
     )
 
-    # Run pre processing
-    run_pre_processing(**common_input)
+    # Run pre-processing and assert mesh sizes
+    model_path, mesh_vtu, mesh_hdf5 = run_pre_processing_with_common_input(model_path, common_input)
+    assert_mesh_sizes(mesh_vtu, mesh_hdf5, expected_num_points, expected_num_cells)
 
-    # Check that mesh files are created
-    assert mesh_path_vtu.is_file(), f"VTU mesh file not found at {mesh_path_vtu}"
-    assert mesh_path_hdf5.is_file(), f"HDF5 mesh file not found at {mesh_path_hdf5}"
+    # Test edge length evaluation
+    edge_length_mesh_path = model_path.with_name(model_path.stem + "_edge_length.xdmf")
     assert edge_length_mesh_path.is_file(), f"Edge length mesh file not found at {edge_length_mesh_path}"
-
-    # Check that mesh files are not empty and have expected sizes
-    mesh_vtu = read_polydata(str(mesh_path_vtu))
-    mesh_hdf5 = Mesh()
-    try:
-        hdf5_file = HDF5File(mesh_hdf5.mpi_comm(), str(mesh_path_hdf5), "r")
-        hdf5_file.read(mesh_hdf5, "/mesh", False)
-    except Exception as e:
-        print(f"Error reading HDF5 mesh: {e}")
 
     try:
         V = FunctionSpace(mesh_hdf5, "DG", 0)
@@ -64,12 +96,7 @@ def test_mesh_model_with_one_inlet(tmpdir):
     except Exception as e:
         print(f"Error reading edge length mesh: {e}")
 
-    assert mesh_vtu.GetNumberOfPoints() == expected_num_points, \
-        f"VTU mesh has {mesh_vtu.GetNumberOfPoints()} points, expected {expected_num_points}"
-    assert mesh_hdf5.num_cells() == expected_num_cells, \
-        f"HDF5 mesh has {mesh_hdf5.num_cells()} cells, expected {expected_num_cells}"
-    assert v.vector().min() > 0, \
-        "Edge length mesh has negative values"
+    assert v.vector().min() > 0, "Edge length mesh has negative values"
 
 
 def test_mesh_model_with_one_inlet_and_one_outlet(tmpdir):
@@ -78,12 +105,9 @@ def test_mesh_model_with_one_inlet_and_one_outlet(tmpdir):
     """
     # Define test data paths
     original_model_path = Path("tests/test_data/cylinder/cylinder.vtp")
-    model_path = Path(tmpdir) / original_model_path.name
-    mesh_path_vtu = model_path.with_suffix(".vtu")
-    mesh_path_hdf5 = model_path.with_suffix(".h5")
 
     # Copy the original model to tmpdir
-    model_path.write_text(original_model_path.read_text())
+    model_path = copy_original_model_to_tmpdir(original_model_path, tmpdir)
 
     # Define expected values
     expected_num_points = 2153
@@ -104,26 +128,9 @@ def test_mesh_model_with_one_inlet_and_one_outlet(tmpdir):
         )
     )
 
-    # Run pre processing
-    run_pre_processing(**common_input)
-
-    # Check that mesh files are created
-    assert mesh_path_vtu.is_file(), f"VTU mesh file not found at {mesh_path_vtu}"
-    assert mesh_path_hdf5.is_file(), f"HDF5 mesh file not found at {mesh_path_hdf5}"
-
-    # Check that mesh files are not empty and have expected sizes
-    mesh_vtu = read_polydata(str(mesh_path_vtu))
-    mesh_hdf5 = Mesh()
-    try:
-        hdf5_file = HDF5File(mesh_hdf5.mpi_comm(), str(mesh_path_hdf5), "r")
-        hdf5_file.read(mesh_hdf5, "/mesh", False)
-    except Exception as e:
-        print(f"Error reading HDF5 mesh: {e}")
-
-    assert mesh_vtu.GetNumberOfPoints() == expected_num_points, \
-        f"VTU mesh has {mesh_vtu.GetNumberOfPoints()} points, expected {expected_num_points}"
-    assert mesh_hdf5.num_cells() == expected_num_cells, \
-        f"HDF5 mesh has {mesh_hdf5.num_cells()} cells, expected {expected_num_cells}"
+    # Run pre-processing and assert mesh sizes
+    model_path, mesh_vtu, mesh_hdf5 = run_pre_processing_with_common_input(model_path, common_input)
+    assert_mesh_sizes(mesh_vtu, mesh_hdf5, expected_num_points, expected_num_cells)
 
 
 def test_mesh_model_with_one_inlet_and_two_outlets(tmpdir):
@@ -132,12 +139,9 @@ def test_mesh_model_with_one_inlet_and_two_outlets(tmpdir):
     """
     # Define test data paths
     original_model_path = Path("tests/test_data/artery/artery.stl")
-    model_path = Path(tmpdir) / original_model_path.name
-    mesh_path_vtu = model_path.with_suffix(".vtu")
-    mesh_path_hdf5 = model_path.with_suffix(".h5")
 
     # Copy the original model to tmpdir
-    model_path.write_text(original_model_path.read_text())
+    model_path = copy_original_model_to_tmpdir(original_model_path, tmpdir)
 
     # Define expected values
     expected_num_points = 5860
@@ -158,26 +162,9 @@ def test_mesh_model_with_one_inlet_and_two_outlets(tmpdir):
         )
     )
 
-    # Run pre processing
-    run_pre_processing(**common_input)
-
-    # Check that mesh files are created
-    assert mesh_path_vtu.is_file(), f"VTU mesh file not found at {mesh_path_vtu}"
-    assert mesh_path_hdf5.is_file(), f"HDF5 mesh file not found at {mesh_path_hdf5}"
-
-    # Check that mesh files are not empty and have expected sizes
-    mesh_vtu = read_polydata(str(mesh_path_vtu))
-    mesh_hdf5 = Mesh()
-    try:
-        hdf5_file = HDF5File(mesh_hdf5.mpi_comm(), str(mesh_path_hdf5), "r")
-        hdf5_file.read(mesh_hdf5, "/mesh", False)
-    except Exception as e:
-        print(f"Error reading HDF5 mesh: {e}")
-
-    assert mesh_vtu.GetNumberOfPoints() == expected_num_points, \
-        f"VTU mesh has {mesh_vtu.GetNumberOfPoints()} points, expected {expected_num_points}"
-    assert mesh_hdf5.num_cells() == expected_num_cells, \
-        f"HDF5 mesh has {mesh_hdf5.num_cells()} cells, expected {expected_num_cells}"
+    # Run pre-processing and assert mesh sizes
+    model_path, mesh_vtu, mesh_hdf5 = run_pre_processing_with_common_input(model_path, common_input)
+    assert_mesh_sizes(mesh_vtu, mesh_hdf5, expected_num_points, expected_num_cells)
 
 
 def test_mesh_model_with_variable_mesh_density(tmpdir):
@@ -186,16 +173,14 @@ def test_mesh_model_with_variable_mesh_density(tmpdir):
     """
     # Define test data paths
     original_model_path = Path("tests/test_data/artery/artery.stl")
-    model_path = Path(tmpdir) / original_model_path.name
-    sphere_file_path = original_model_path.with_name("stored_" + model_path.stem +
+    sphere_file_path = original_model_path.with_name("stored_" + original_model_path.stem +
                                                      "_variable_mesh_density_distance_to_sphere_spheres.vtp")
+
+    # Copy the original model to tmpdir
+    model_path = copy_original_model_to_tmpdir(original_model_path, tmpdir)
+
+    # Copy the sphere file to tmpdir
     copied_sphere_file_path = model_path.with_name(model_path.stem + "_distance_to_sphere_spheres.vtp")
-
-    mesh_path_vtu = model_path.with_suffix(".vtu")
-    mesh_path_hdf5 = model_path.with_suffix(".h5")
-
-    # Copy the original model and sphere file to tmpdir
-    model_path.write_text(original_model_path.read_text())
     copied_sphere_file_path.write_text(sphere_file_path.read_text())
 
     # Define expected values
@@ -217,26 +202,9 @@ def test_mesh_model_with_variable_mesh_density(tmpdir):
         )
     )
 
-    # Run pre processing
-    run_pre_processing(**common_input)
-
-    # Check that mesh files are created
-    assert mesh_path_vtu.is_file(), f"VTU mesh file not found at {mesh_path_vtu}"
-    assert mesh_path_hdf5.is_file(), f"HDF5 mesh file not found at {mesh_path_hdf5}"
-
-    # Check that mesh files are not empty and have expected sizes
-    mesh_vtu = read_polydata(str(mesh_path_vtu))
-    mesh_hdf5 = Mesh()
-    try:
-        hdf5_file = HDF5File(mesh_hdf5.mpi_comm(), str(mesh_path_hdf5), "r")
-        hdf5_file.read(mesh_hdf5, "/mesh", False)
-    except Exception as e:
-        print(f"Error reading HDF5 mesh: {e}")
-
-    assert mesh_vtu.GetNumberOfPoints() == expected_num_points, \
-        f"VTU mesh has {mesh_vtu.GetNumberOfPoints()} points, expected {expected_num_points} points"
-    assert mesh_hdf5.num_cells() == expected_num_cells, \
-        f"HDF5 mesh has {mesh_hdf5.num_cells()} cells, expected {expected_num_cells} cells"
+    # Run pre-processing and assert mesh sizes
+    model_path, mesh_vtu, mesh_hdf5 = run_pre_processing_with_common_input(model_path, common_input)
+    assert_mesh_sizes(mesh_vtu, mesh_hdf5, expected_num_points, expected_num_cells)
 
 
 def compute_cylinder_diameter_at_cut(mesh, point_coords, plane_normal):
@@ -272,16 +240,14 @@ def test_mesh_model_with_variable_solid_thickness(tmpdir):
     """
     # Define test data paths
     original_model_path = Path("tests/test_data/cylinder/cylinder.vtp")
-    model_path = Path(tmpdir) / original_model_path.name
-    sphere_file_path = original_model_path.with_name("stored_" + model_path.stem +
+    sphere_file_path = original_model_path.with_name("stored_" + original_model_path.stem +
                                                      "_variable_solid_thickness_distance_to_sphere_solid_thickness.vtp")
+
+    # Copy the original model to tmpdir
+    model_path = copy_original_model_to_tmpdir(original_model_path, tmpdir)
+
+    # Copy the sphere file to tmpdir
     copied_sphere_file_path = model_path.with_name(model_path.stem + "_distance_to_sphere_solid_thickness.vtp")
-
-    mesh_path_vtu = model_path.with_suffix(".vtu")
-    mesh_path_hdf5 = model_path.with_suffix(".h5")
-
-    # Copy the original model and sphere file to tmpdir
-    model_path.write_text(original_model_path.read_text())
     copied_sphere_file_path.write_text(sphere_file_path.read_text())
 
     # Define expected values
@@ -307,26 +273,9 @@ def test_mesh_model_with_variable_solid_thickness(tmpdir):
         )
     )
 
-    # Run pre processing
-    run_pre_processing(**common_input)
-
-    # Check that mesh files are created
-    assert mesh_path_vtu.is_file(), f"VTU mesh file not found at {mesh_path_vtu}"
-    assert mesh_path_hdf5.is_file(), f"HDF5 mesh file not found at {mesh_path_hdf5}"
-
-    # Check that mesh files are not empty and have expected sizes
-    mesh_vtu = read_polydata(str(mesh_path_vtu))
-    mesh_hdf5 = Mesh()
-    try:
-        hdf5_file = HDF5File(mesh_hdf5.mpi_comm(), str(mesh_path_hdf5), "r")
-        hdf5_file.read(mesh_hdf5, "/mesh", False)
-    except Exception as e:
-        print(f"Error reading HDF5 mesh: {e}")
-
-    assert mesh_vtu.GetNumberOfPoints() == expected_num_points, \
-        f"VTU mesh has {mesh_vtu.GetNumberOfPoints()} points, expected {expected_num_points} points"
-    assert mesh_hdf5.num_cells() == expected_num_cells, \
-        f"HDF5 mesh has {mesh_hdf5.num_cells()} cells, expected {expected_num_cells} cells"
+    # Run pre processing and assert mesh sizes
+    model_path, mesh_vtu, mesh_hdf5 = run_pre_processing_with_common_input(model_path, common_input)
+    assert_mesh_sizes(mesh_vtu, mesh_hdf5, expected_num_points, expected_num_cells)
 
     # Compute diameter at inlet and outlet
     diameter_at_inlet = compute_cylinder_diameter_at_cut(mesh_vtu, [0, -3.1, 0], [0, 1, 0])
