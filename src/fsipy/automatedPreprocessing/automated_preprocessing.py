66262dd586c372320b8075b059c260207f8da220
johannesring <johannes.ring@gmail.com>
2023-10-25 13:11:50 +0200

    Updates for latest VaMPy

--- src/fsipy/automatedPreprocessing/automated_preprocessing.py
+++ src/fsipy/automatedPreprocessing/automated_preprocessing.py
@@ -11,11 +11,11 @@ from morphman import get_uncapped_surface, write_polydata, get_parameters, vtk_c
     create_new_surface, compute_centers, vmtk_smooth_surface, str2bool, vmtk_compute_voronoi_diagram, \
     prepare_output_surface, vmtk_compute_geometric_features
 
-from vampy.automatedPreprocessing import ToolRepairSTL
 from vampy.automatedPreprocessing.preprocessing_common import read_polydata, get_centers_for_meshing, \
     dist_sphere_diam, dist_sphere_curvature, dist_sphere_constant, get_regions_to_refine, add_flow_extension, \
     write_mesh, mesh_alternative, find_boundaries, compute_flow_rate, setup_model_network, \
     radiusArrayName, scale_surface, get_furtest_surface_point, check_if_closed_surface
+from vampy.automatedPreprocessing.repair_tools import find_and_delete_nan_triangles, clean_surface, print_surface_info
 from vampy.automatedPreprocessing.simulate import run_simulation
 from vampy.automatedPreprocessing.visualize import visualize_model
 
@@ -152,10 +152,10 @@ def run_pre_processing(input_model, verbose_print, smoothing_method, smoothing_f
         surface = vtk_triangulate_surface(surface)
 
         # Check the mesh if there is redundant nodes or NaN triangles.
-        ToolRepairSTL.surfaceOverview(surface)
-        ToolRepairSTL.foundAndDeleteNaNTriangles(surface)
-        surface = ToolRepairSTL.cleanTheSurface(surface)
-        foundNaN = ToolRepairSTL.foundAndDeleteNaNTriangles(surface)
+        print_surface_info(surface)
+        find_and_delete_nan_triangles(surface)
+        surface = clean_surface(surface)
+        foundNaN = find_and_delete_nan_triangles(surface)
         if foundNaN:
             raise RuntimeError(("There is an issue with the surface. "
                                 "Nan coordinates or some other shenanigans."))
@@ -180,6 +180,7 @@ def run_pre_processing(input_model, verbose_print, smoothing_method, smoothing_f
 
     centerlines, voronoi, _ = compute_centerlines(source, target, file_name_centerlines, capped_surface,
                                                   resampling=resampling_step)
+    print("\n")
     tol = get_centerline_tolerance(centerlines)
 
     # Get 'center' and 'radius' of the regions(s)
@@ -503,7 +504,8 @@ def run_pre_processing(input_model, verbose_print, smoothing_method, smoothing_f
         print("--- Evaluating edge length\n")
         edge_length_evaluator(file_name_xdmf_mesh, file_name_edge_length_xdmf)
 
-    network, probe_points = setup_model_network(centerlines, file_name_probe_points, region_center, verbose_print)
+    network, probe_points = setup_model_network(centerlines, file_name_probe_points, region_center, verbose_print,
+                                                has_outlet, has_multiple_inlets)
 
     # Load updated parameters following meshing
     parameters = get_parameters(base_path)
