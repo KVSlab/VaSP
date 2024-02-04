2cc8200f35fb1c50088b38c453d38e97f8709351
Kei <withwhom0215@gmail.com>
2024-02-04 13:17:54 +0100

    rename to vasp under postprocessing_mesh

--- src/vasp/automatedPostprocessing/postprocessing_mesh/separate_mesh.py
+++ src/vasp/automatedPostprocessing/postprocessing_mesh/separate_mesh.py
@@ -8,7 +8,7 @@ import h5py
 from pathlib import Path
 from typing import Union
 
-from fsipy.automatedPostprocessing.postprocessing_mesh import postprocessing_mesh_common
+from vasp.automatedPostprocessing.postprocessing_mesh import postprocessing_mesh_common
 
 from dolfin import MPI, Mesh, MeshFunction, HDF5File, SubMesh, File
 
