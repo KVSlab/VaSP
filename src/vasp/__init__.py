5fc5a8209cdfe2d55693d1a7287bf54720c81eb2
Kei <withwhom0215@gmail.com>
2024-07-01 19:48:39 +0200

    fix issue with importlib

--- src/vasp/__init__.py
+++ src/vasp/__init__.py
@@ -4,7 +4,7 @@ from importlib.metadata import metadata
 
 meta = metadata("vasp")
 __version__ = meta["Version"]
-__author__ = meta["Author"]
+__author__ = meta["Author-email"]
 __license__ = meta["License"]
 __email__ = meta["Author-email"]
 __program_name__ = meta["Name"]
